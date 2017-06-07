ab0eae9d4c5422d54d8425152d09a5b7185b9e43
vijay mali <guddavijay@gmail.com>
2017-06-07 13:44:53 +0530

    form field added in lib

---
+++ lib/Form/Field/Password.php
@@ -0,0 +1,22 @@
+<?php
+namespace xavoc\mlm;
+
+class Form_Field_Password extends \Form_Field_Password {
+    
+    function validate(){
+        // empty value is allowed        
+        if($this->value!=''){
+            if(strlen($this->value) < 6) {
+                $this->displayFieldError('Must not be less than 6 characters');
+            }
+        }
+        if($this->value != $this->owner->get('re_password'))
+            $this->owner->getElement('re_password')->displayFieldError('Password doesn\'t match');
+        return parent::validate();
+    }
+
+    function render(){
+    	$this->js(true)->_load('pwstrength-bootstrap-1.2.5.min')->pwstrength();
+    	parent::render();
+    }
+}
