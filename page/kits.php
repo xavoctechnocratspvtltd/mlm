e49994e31e20437c70b3faa42c8927eac680b5b8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-06-07 23:36:16 +0530

    setup is now in controller

--- page/kits.php
+++ page/kits.php
@@ -12,7 +12,6 @@ class page_kits extends \xepan\base\Page {
 		$crud = $this->add('CRUD');
 		$model= $this->add('xavoc\mlm\Model_Kit');
 		$crud->setModel($model,['qty_unit','name','sku','display_sequence','original_price','sale_price','pv','bv','sv','capping','introducer_income','description']);
-		$crud->addRef('xavoc\mlm\KitItem',['label'=>'Item']);		
 	}
 
 }
