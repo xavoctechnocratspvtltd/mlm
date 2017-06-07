2ed4e49b00fb89014fa3bb1473f51cc7fe6b9e36
sinharakesh <rksinha.btech@gmail.com>
2017-06-07 17:19:11 +0530

    working on progress

--- lib/Tool/DashBoard.php
+++ lib/Tool/DashBoard.php
@@ -16,6 +16,7 @@ class Tool_DashBoard extends \xepan\cms\View_Tool{
 		if($this->options['show-status']){
 		}
 
+		$this->add('xavoc\mlm\View_ProfileChecker');
 		$this->add('View_Info')->set('Dashboard Tool');
 	}
 }
---
+++ lib/View/ProfileChecker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace xavoc\mlm;
+
+/**
+* it checks profile updated in percentage
+*/
+
+class View_ProfileChecker extends \xepan\cms\View_Tool{
+	
+	function init(){
+		parent::init();
+
+		// check kit is purchased or not
+		// check kyc form is updated or not
+
+	}
+
+	// function defaultTemplate(){
+	// 	return ['xavoc\view\profilechecker'];
+	// }
+}
