3707daa9253e14fd7821adde41f556076ab0c290
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-06-07 20:59:28 +0530

    update structure file

--- mlm_structure.sql
+++ mlm_structure.sql
@@ -11,20 +11,11 @@
  Target Server Version : 100118
  File Encoding         : utf-8
 
- Date: 06/07/2017 17:15:38 PM
+ Date: 06/07/2017 20:59:16 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
---  Table structure for `mlm_closing`
--- ----------------------------
-DROP TABLE IF EXISTS `mlm_closing`;
-CREATE TABLE `mlm_closing` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 --  Table structure for `mlm_distributor`
@@ -54,8 +45,10 @@ CREATE TABLE `mlm_distributor` (
   `nominee_age` varchar(255) DEFAULT NULL,
   `weekly_intros_amount` int(11) DEFAULT NULL,
   `total_intros_amount` int(11) DEFAULT NULL,
-  `weekly_left_sv` int(11) DEFAULT NULL,
-  `weekly_right_sv` int(11) DEFAULT NULL,
+  `day_left_sv` int(11) DEFAULT NULL,
+  `day_right_sv` int(11) DEFAULT NULL,
+  `day_pais` int(11) DEFAULT NULL,
+  `week_pairs` int(11) DEFAULT NULL,
   `total_left_sv` int(11) DEFAULT NULL,
   `total_right_sv` int(11) DEFAULT NULL,
   `monthly_left_dp_mrp_diff` decimal(10,2) DEFAULT NULL,
@@ -64,6 +57,7 @@ CREATE TABLE `mlm_distributor` (
   `carried_amount` decimal(10,2) DEFAULT NULL,
   `greened_on` datetime DEFAULT NULL,
   `ansestors_updated` tinyint(4) DEFAULT NULL,
+  `temp` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=138 DEFAULT CHARSET=utf8;
 
@@ -81,36 +75,6 @@ CREATE TABLE `mlm_generation_business` (
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `mlm_kit`
--- ----------------------------
-DROP TABLE IF EXISTS `mlm_kit`;
-CREATE TABLE `mlm_kit` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `pv` int(11) DEFAULT NULL,
-  `bv` int(11) DEFAULT NULL,
-  `sv` int(11) DEFAULT NULL,
-  `capping` int(11) DEFAULT NULL,
-  `introducer_income` int(11) DEFAULT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `display_sequence` varchar(255) DEFAULT NULL,
-  `description` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- ----------------------------
---  Table structure for `mlm_kit_item_asso`
--- ----------------------------
-DROP TABLE IF EXISTS `mlm_kit_item_asso`;
-CREATE TABLE `mlm_kit_item_asso` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mlm_kit_id` int(11) DEFAULT NULL,
-  `mlm_item_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
--- ----------------------------
 --  Table structure for `mlm_kyc`
 -- ----------------------------
 DROP TABLE IF EXISTS `mlm_kyc`;
@@ -122,5 +86,14 @@ CREATE TABLE `mlm_kyc` (
   `aadhar_image_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `mlm_payout`
+-- ----------------------------
+DROP TABLE IF EXISTS `mlm_payout`;
+CREATE TABLE `mlm_payout` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
