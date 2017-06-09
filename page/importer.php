2a0538fe8612795d0a2ab74a1791314232335eed
sinharakesh <rksinha.btech@gmail.com>
2017-06-09 21:42:01 +0530

    importer in commit roll back

--- page/importer.php
+++ page/importer.php
@@ -39,89 +39,99 @@ class page_importer extends \xepan\base\Page {
 			$importer = new \xepan\base\CSVImporter($path,true,',');
 			$csv_data = $importer->get();
 
-			$this->add('xavoc\mlm\Controller_Setup');
+			try{
 
-			// [old_dis_id => new_dis_id,old_dis_id => new_dis_id]
-			$root_ds = $this->add('xavoc\mlm\Model_Distributor')->loadRoot();
-			$all_package = [];
-			$dis_having_not_kit = [];			
-			$kits = $this->add('xavoc\mlm\Model_Kit');
-			foreach ($kits as $key => $kit) {
-				$all_package[$kit['code']] = $kit['id'];
-			}
+				$this->api->db->beginTransaction();
 
-			$dis_id_mapping = [1=>$root_ds->id];
-			// old_plan_name => new package_code
-			$package_mapping = [
-							'Registration'=>'package1',
-							'801'=>'package2',
-							'1000'=>'package3',
-						];
+				$this->add('xavoc\mlm\Controller_Setup');
 
-			// throw new \Exception("Error Processing Request", 1);
-			$count = 0;
-			foreach ($csv_data as $key => $old_dis) {
-				if($count > 1) break;
-
-				$data = [];
-				$data['sponsor_id'] =  $dis_id_mapping[$old_dis['Upline_PrimaryKey']];
-				$data['introducer_id'] =  $dis_id_mapping[$old_dis['sponsorid_Key']];
-				$data['first_name'] = $old_dis['fname'];
-				$data['address'] = $old_dis['address'];
-
-				$data['city'] = $old_dis['city'];
-
-				$country = $this->add('xepan\base\Model_Country');
-				$country->addCondition('name', ucwords($data['country']));
-				$country->tryLoadAny();
-				if($country->loaded())
-					$data['country_id'] = $country->id;
-				
-				$state = $this->add('xepan\base\Model_State');
-				$state->addCondition('name', ucwords($data['state']));
-				$state->tryLoadAny();
-				if($state->loaded())
-					$data['state_id'] = $state->id;
-
-				$data['pin_code'] = $old_dis['pincode'];
-
-				$data['mobile_number'] = $old_dis['mobileno'];
-				$data['email'] = $old_dis['email'];
-
-				$data['created_at'] = date('Y-m-d H:i:s', strtotime($old_dis['joiningdate']));
-				$data['greened_on'] = date('Y-m-d H:i:s', strtotime($old_dis['topupeddate']));
-
-				$data['d_bank_name'] = $old_dis['bankname'];
-				$data['d_account_number'] = $old_dis['accountno'];
-				$data['d_bank_ifsc_code'] = $old_dis['ifsccode'];
-				$data['pan_no'] = $old_dis['panno'];
-
-				$data['username'] = $old_dis['Member_Username'];
-				$data['password'] = $old_dis['password'];
-
-				$data['day_left_sv'] = $data['total_left_sv'] = $old_dis['leftpoint'];
-				$data['day_right_sv'] = $data['total_right_sv'] = $old_dis['rightpoint'];
-
-				if($old_dis['Position']== 'L')
-					$data['side'] =  'A';
-				else
-					$data['side'] =  'B';
-
-				$distributor = $this->add('xavoc\mlm\Model_Distributor');
-				$distributor->register($data);
-
-				$kit_code = $package_mapping[trim($data['planname'])];
-				$kit_id =  isset($all_package[$kit_code])?$all_package[$kit_code]:0;
-				if($kit_id){
-					$distributor->purchaseKit($kit_id);
-				}else{
-					$dis_having_not_kit[] = $distributor->id;
+				// [old_dis_id => new_dis_id,old_dis_id => new_dis_id]
+				$root_ds = $this->add('xavoc\mlm\Model_Distributor')->loadRoot();
+				$all_package = [];
+				$dis_having_not_kit = [];			
+				$kits = $this->add('xavoc\mlm\Model_Kit');
+				foreach ($kits as $key => $kit) {
+					$all_package[$kit['code']] = $kit['id'];
 				}
 
-				if(!isset($dis_id_mapping[$old_dis['Member_PrimaryKey']]))
-					$dis_id_mapping[$old_dis['Member_PrimaryKey']] = $distributor->id;
+				$dis_id_mapping = [1=>$root_ds->id];
+				// old_plan_name => new package_code
+				$package_mapping = [
+								'Registration'=>'package1',
+								'801'=>'package2',
+								'1000'=>'package3',
+							];
 
-				$count++;
+				// throw new \Exception("Error Processing Request", 1);
+				$count = 0;
+				foreach ($csv_data as $key => $old_dis) {
+					if($count > 1) break;
+
+					$data = [];
+					$data['sponsor_id'] =  $dis_id_mapping[$old_dis['Upline_PrimaryKey']];
+					$data['introducer_id'] =  $dis_id_mapping[$old_dis['sponsorid_Key']];
+					$data['first_name'] = $old_dis['fname'];
+					$data['address'] = $old_dis['address'];
+
+					$data['city'] = $old_dis['city'];
+
+					$country = $this->add('xepan\base\Model_Country');
+					$country->addCondition('name', ucwords($data['country']));
+					$country->tryLoadAny();
+					if($country->loaded())
+						$data['country_id'] = $country->id;
+					
+					$state = $this->add('xepan\base\Model_State');
+					$state->addCondition('name', ucwords($data['state']));
+					$state->tryLoadAny();
+					if($state->loaded())
+						$data['state_id'] = $state->id;
+
+					$data['pin_code'] = $old_dis['pincode'];
+
+					$data['mobile_number'] = $old_dis['mobileno'];
+					$data['email'] = $old_dis['email'];
+
+					$data['created_at'] = date('Y-m-d H:i:s', strtotime($old_dis['joiningdate']));
+					$data['greened_on'] = date('Y-m-d H:i:s', strtotime($old_dis['topupeddate']));
+
+					$data['d_bank_name'] = $old_dis['bankname'];
+					$data['d_account_number'] = $old_dis['accountno'];
+					$data['d_bank_ifsc_code'] = $old_dis['ifsccode'];
+					$data['pan_no'] = $old_dis['panno'];
+
+					$data['username'] = $old_dis['Member_Username'];
+					$data['password'] = $old_dis['password'];
+
+					$data['day_left_sv'] = $data['total_left_sv'] = $old_dis['leftpoint'];
+					$data['day_right_sv'] = $data['total_right_sv'] = $old_dis['rightpoint'];
+
+					if($old_dis['Position']== 'L')
+						$data['side'] =  'A';
+					else
+						$data['side'] =  'B';
+
+					$distributor = $this->add('xavoc\mlm\Model_Distributor');
+					$distributor->register($data);
+
+					$kit_code = $package_mapping[trim($data['planname'])];
+					$kit_id =  isset($all_package[$kit_code])?$all_package[$kit_code]:0;
+					if($kit_id){
+						$distributor->purchaseKit($kit_id);
+					}else{
+						$dis_having_not_kit[] = $distributor->id;
+					}
+
+					if(!isset($dis_id_mapping[$old_dis['Member_PrimaryKey']]))
+						$dis_id_mapping[$old_dis['Member_PrimaryKey']] = $distributor->id;
+
+					$count++;
+				}
+
+				$this->api->db->commit();
+			}catch(\Exception $e){
+				$this->api->db->rollback();
+				throw new \Exception($e->getMessage());
 			}
 
 			// echo "<pre>";
