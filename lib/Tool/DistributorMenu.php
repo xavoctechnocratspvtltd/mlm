b428185426f096563c2bd5137aafb2c9a04bda35
sinharakesh <rksinha.btech@gmail.com>
2017-06-09 10:18:46 +0530

    distributor menu corrected

--- lib/Tool/DistributorMenu.php
+++ lib/Tool/DistributorMenu.php
@@ -8,12 +8,24 @@ class Tool_DistributorMenu extends \xepan\cms\View_Tool{
 	function init(){
 		parent::init();
 
+		$this->distributor = $distributor = $this->add('xavoc\mlm\Model_Distributor');
+		$distributor->addExpression('attachment_count')->set(function($m,$q){
+			return $q->expr('IFNULL([0],0)',[$m->refSQL('xavoc\mlm\Attachment')->count()]);
+		});
+
+		$distributor->loadLoggedIn();
+		if(!$distributor->loaded()){
+			return "distributor not found";
+		}
+
 		$menu = [
 				['key'=>'dashboard','name'=>'Dashboard'],
 				['key'=>'panelregistration', 'name'=>'Registration'],
 				['key'=>'genology','name'=>'Genology'],
 				['key'=>'mypayouts','name'=>'My Payouts'],
-				['key'=>'mywallet','name'=>'My Wallet']
+				['key'=>'mywallet','name'=>'My Wallet'],
+				['key'=>'myorder','name'=>'My Orders'],
+				['key'=>'setting','name'=>'Settings'],
 			];
 
 		$this->complete_lister = $cl = $this->add('CompleteLister',null,null,['view/distributormenu']);
@@ -26,6 +38,8 @@ class Tool_DistributorMenu extends \xepan\cms\View_Tool{
 				$g->current_row_html['active_menu'] = "deactive";
 		});
 
-		
+		$cl->template->trySet('distributor_name',$distributor['name']);
+		$cl->template->trySet('distributor_dp',($distributor['image']?:"websites/dsmarketing/www/img/100.svg"));
+
 	}
 }
