afb84f6cbd6b97a98de979d8a1a3bd89c7ea09a3
sinharakesh <rksinha.btech@gmail.com>
2017-06-10 10:39:34 +0530

    tool register new field and it's template corrected with error display

--- lib/Tool/Register.php
+++ lib/Tool/Register.php
@@ -18,22 +18,26 @@ class Tool_Register extends \xepan\cms\View_Tool{
 	function init(){
 		parent::init();
 
+		$form_field = ['introducer_id','side','first_name','last_name','dob','email','mobile_number','pan_no','country_id','state_id','city','pin_code','address'];
 		$form = $this->add('Form');
 		$form->setLayout(['view/form/registration']);
-		$form->setModel('xavoc\mlm\Distributor',['first_name','last_name','introducer_id','side','email','mobile_number','pan_no','address','dob','username','password']);
-		$form->getElement('first_name')->validate('required');
-		$form->getElement('last_name')->validate('required');
-		$form->getElement('introducer_id')->validate('required');
-		$form->getElement('side')->validate('required');
-		$form->getElement('dob')->validate('required');
-		$form->getElement('pan_no')->validate('required');
-		$form->getElement('email')->validate('required');
-		$form->getElement('mobile_number')->validate('required');
-		$form->getElement('address')->validate('required');
+		$form->setModel('xavoc\mlm\Distributor',$form_field);
+		
+		foreach ($form_field as $key => $name) {
+			$form->getElement($name)->validate('required');
+		}
+		
+		$country_field = $form->getElement('country_id');
+		$state_field = $form->getElement('state_id');
+		if($_GET['country_id']){
+			$state_field->getModel()->addCondition('country_id',$_GET['country_id']);
+		}
+		$country_field->js('change',$state_field->js()->reload(null,null,[$this->app->url(null,['cut_object'=>$state_field->name]),'country_id'=>$country_field->js()->val()]));
+		// $country_field->js('change',$form->js()->atk4_form('reloadField','state_id',[$this->app->url(null,['cut_object'=>$state_field->name]),'country_id'=>$country_field->js()->val()]));
+		
+
 		$form->addField('username')->validate('required|email');
-		$form->addField('password','password')->validate('required');
-		// $form->addField('xepan\base\DropDownNormal','sponsor')->setModel('xavoc\mlm\Distributor');
-		// $form->addField('xepan\base\DropDownNormal','introducer')->setModel('xavoc\mlm\Distributor');
+		$form->addField('password','password')->validate('required');	
 		$form->addSubmit('Register')->addClass(' btn btn-primary btn-block');
 		
 		if($form->isSubmitted()){
