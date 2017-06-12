31dafe21f8b250373915c39bea0280012cbc3ae2
vijay mali <guddavijay@gmail.com>
2017-06-12 12:45:30 +0530

    distributor green & deactivate email & sms send function created

--- lib/Model/Distributor.php
+++ lib/Model/Distributor.php
@@ -237,13 +237,19 @@ class Model_Distributor extends \xepan\commerce\Model_Customer {
 			[
 				'fields'=>[
 							'welcome_mail_subject'=>'Line',
-							'welcome_mail_content'=>'Text',
+							'welcome_mail_content'=>'xepan\base\RichText',
 							'welcome_sms_content'=>'Text',
+							'green_mail_subject'=>'Line',
+							'green_mail_content'=>'xepan\base\RichText',
+							'green_sms_content'=>'Text',
+							'deactivate_mail_subject'=>'Line',
+							'deactivate_mail_content'=>'xepan\base\RichText',
+							'deactivate_sms_content'=>'Text',
 							'payout_mail_subject'=>'Line',
-							'payout_mail_content'=>'Text',
+							'payout_mail_content'=>'xepan\base\RichText',
 							'payout_sms_content'=>'Text',
 							'topup_mail_subject'=>'Line',
-							'topup_mail_content'=>'Text',
+							'topup_mail_content'=>'xepan\base\RichText',
 							'topup_sms_content'=>'Text',
 						],
 					'config_key'=>'DM_WELCOME_CONTENT',
@@ -300,6 +306,162 @@ class Model_Distributor extends \xepan\commerce\Model_Customer {
 		}
 	}
 
+	// send email and send sms
+	function sendMailGreenDistributor(){
+		if(!$this->loaded()) throw new \Exception("Distributor Not Found, some thing wrong");
+				
+		// welcome mail and sms
+		$green_config = $this->add('xepan\base\Model_ConfigJsonModel',
+			[
+				'fields'=>[
+							'welcome_mail_subject'=>'Line',
+							'welcome_mail_content'=>'xepan\base\RichText',
+							'welcome_sms_content'=>'Text',
+							'green_mail_subject'=>'Line',
+							'green_mail_content'=>'xepan\base\RichText',
+							'green_sms_content'=>'Text',
+							'deactivate_mail_subject'=>'Line',
+							'deactivate_mail_content'=>'xepan\base\RichText',
+							'deactivate_sms_content'=>'Text',
+							'payout_mail_subject'=>'Line',
+							'payout_mail_content'=>'xepan\base\RichText',
+							'payout_sms_content'=>'Text',
+							'topup_mail_subject'=>'Line',
+							'topup_mail_content'=>'xepan\base\RichText',
+							'topup_sms_content'=>'Text',
+						],
+					'config_key'=>'DM_WELCOME_CONTENT',
+					'application'=>'mlm'
+			]);
+		$green_config->tryLoadAny();
+		
+
+		// Send Email
+			// subject
+		if($this->app->getConfig('send_email')){
+
+			if(!$green_config['green_mail_subject'] OR !$green_config['green_mail_content']) throw new \Exception("plase update Green Distributor mail content");
+			$temp = $this->add('GiTemplate');
+			$temp->loadTemplateFromString($green_config['green_mail_subject']);
+			$temp->set($this->data);
+			$subject = $temp->render();
+				// body
+			$temp = $this->add('GiTemplate');
+			$temp->loadTemplateFromString($green_config['green_mail_content']);
+			$temp->set($this->data);
+			$body = $temp->render();
+
+			$email_setting = $this->add('xepan\communication\Model_Communication_EmailSetting')->setOrder('id','asc');
+			$email_setting->addCondition('is_active',true);
+			$email_setting->tryLoadAny();
+
+			if(!$email_setting->loaded()) throw new \Exception("update your email setting ", 1);
+
+
+			$communication = $this->add('xepan\communication\Model_Communication_Abstract_Email');
+			$communication->addCondition('communication_type','Email');
+
+			$communication->getElement('status')->defaultValue('Draft');
+			$communication['direction']='Out';
+			$communication->setfrom($email_setting['from_email'],$email_setting['from_name']);
+			
+			$communication->addTo($this['email']);
+			$communication->setSubject($subject);
+			$communication->setBody($body);
+			$communication->send($email_setting);
+		}
+
+		// send SMS
+		if($this->app->getConfig('send_sms')){
+			$message = $green_config['green_sms_content'];
+			$temp = $this->add('GiTemplate');
+			$temp->loadTemplateFromString($message);
+			$temp->set($this->data);
+			$message = $temp->render();
+			
+			if(!$green_config['green_sms_content']) throw new \Exception("plase update Green Distributor SMS content");
+			$this->add('xepan\communication\Controller_Sms')->sendMessage($this['mobile_number'],$message);
+		}
+	}
+
+	// send email and send sms
+	function sendMailDeactivateDistributor(){
+		if(!$this->loaded()) throw new \Exception("Distributor Not Found, some thing wrong");
+				
+		// welcome mail and sms
+		$deactivate_config = $this->add('xepan\base\Model_ConfigJsonModel',
+			[
+				'fields'=>[
+							'welcome_mail_subject'=>'Line',
+							'welcome_mail_content'=>'xepan\base\RichText',
+							'welcome_sms_content'=>'Text',
+							'green_mail_subject'=>'Line',
+							'green_mail_content'=>'xepan\base\RichText',
+							'green_sms_content'=>'Text',
+							'deactivate_mail_subject'=>'Line',
+							'deactivate_mail_content'=>'xepan\base\RichText',
+							'deactivate_sms_content'=>'Text',
+							'payout_mail_subject'=>'Line',
+							'payout_mail_content'=>'xepan\base\RichText',
+							'payout_sms_content'=>'Text',
+							'topup_mail_subject'=>'Line',
+							'topup_mail_content'=>'xepan\base\RichText',
+							'topup_sms_content'=>'Text',
+						],
+					'config_key'=>'DM_WELCOME_CONTENT',
+					'application'=>'mlm'
+			]);
+		$deactivate_config->tryLoadAny();
+		
+
+		// Send Email
+			// subject
+		if($this->app->getConfig('send_email')){
+
+			if(!$deactivate_config['deactivate_mail_subject'] OR !$deactivate_config['deactivate_mail_content']) throw new \Exception("plase update Deactivate Distributor mail content");
+			$temp = $this->add('GiTemplate');
+			$temp->loadTemplateFromString($deactivate_config['deactivate_mail_subject']);
+			$temp->set($this->data);
+			$subject = $temp->render();
+				// body
+			$temp = $this->add('GiTemplate');
+			$temp->loadTemplateFromString($deactivate_config['deactivate_mail_content']);
+			$temp->set($this->data);
+			$body = $temp->render();
+
+			$email_setting = $this->add('xepan\communication\Model_Communication_EmailSetting')->setOrder('id','asc');
+			$email_setting->addCondition('is_active',true);
+			$email_setting->tryLoadAny();
+
+			if(!$email_setting->loaded()) throw new \Exception("update your email setting ", 1);
+
+
+			$communication = $this->add('xepan\communication\Model_Communication_Abstract_Email');
+			$communication->addCondition('communication_type','Email');
+
+			$communication->getElement('status')->defaultValue('Draft');
+			$communication['direction']='Out';
+			$communication->setfrom($email_setting['from_email'],$email_setting['from_name']);
+			
+			$communication->addTo($this['email']);
+			$communication->setSubject($subject);
+			$communication->setBody($body);
+			$communication->send($email_setting);
+		}
+
+		// send SMS
+		if($this->app->getConfig('send_sms')){
+			$message = $deactivate_config['deactivate_sms_content'];
+			$temp = $this->add('GiTemplate');
+			$temp->loadTemplateFromString($message);
+			$temp->set($this->data);
+			$message = $temp->render();
+			
+			if(!$deactivate_config['deactivate_sms_content']) throw new \Exception("plase update Deactivate Distributor SMS content");
+			$this->add('xepan\communication\Controller_Sms')->sendMessage($this['mobile_number'],$message);
+		}
+	}
+
 	function beforeDeleteDistributor(){
 		$user_id = $this['user_id'];
 		if($user_id){
@@ -349,6 +511,7 @@ class Model_Distributor extends \xepan\commerce\Model_Customer {
 		$this->updateAnsestorsSV($this['sv']);
 		$this->updateAnsestorsBV($this['bv']);
 		if($introducer  = $this->introducer()) $introducer->addSessionIntro($kit['introducer_income']);
+		$this->sendMailGreenDistributor();
 	}
 
 	function repurchase($bv){
