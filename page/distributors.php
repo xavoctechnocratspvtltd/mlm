334880e1c2ff7e84d42a10e58e8c30b9fa7d2756
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-06-12 21:31:43 +0530

    Single Distributor page

--- page/distributors.php
+++ page/distributors.php
@@ -8,11 +8,23 @@ class page_distributors extends \xepan\base\Page {
 
 	function init(){
 		parent::init();
+		$status_color = [
+						'Red'=>'danger',
+						'KitSelected'=>'default',
+						'KitPaid'=>'primary',
+						'Green'=>'success'
+					];
 
-
-		$crud = $this->add('xepan\hr\CRUD');
-		$crud->setModel('xavoc\mlm\Distributor');
+		$dis_action_m = $this->add('xavoc\mlm\Model_Distributor_Actions');
+		
+		$crud = $this->add('xepan\hr\CRUD',['status_color'=>$status_color,'allow_edit'=>false,'allow_del'=>false, 'allow_add'=>false]);
+		$dis_action_m->add('xavoc\mlm\Controller_SideBarStatusFilter');
+		$crud->setModel($dis_action_m,
+						[''],
+						['name','user','side','sponsor','introducer','joining_date','email','mobile_number']
+					);
 		$crud->grid->addPaginator($ipp=50);
+		$crud->grid->removeColumn('attachment_icon');
 	}
 
 }
