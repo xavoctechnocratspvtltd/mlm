451350d7a24010d72d9a26a156371a23021e680e
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-06-13 17:53:35 +0530

    show only generation lines that has some business

--- lib/Model/Distributor/Genology.php
+++ lib/Model/Distributor/Genology.php
@@ -16,6 +16,7 @@ class Model_Distributor_Genology extends \xavoc\mlm\Model_Distributor
 		$this->addExpression('generation_business')->set(function($m,$q){
 
 			$x = $m->add('xavoc\mlm\Model_GenerationBusiness',['table_alias'=>'generation_business']);
+			$x->addCondition('month_bv','>',0);
 			return $x->addCondition('distributor_id',$q->getField('id'))
 						->_dsql()->del('fields')->field($q->expr('group_concat([0] SEPARATOR ",")',[$x->getElement('name')]));
 		});
