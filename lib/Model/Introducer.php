62435b8b5d41b18bd8766a6a86de7a19767e41b8
vijay mali <guddavijay@gmail.com>
2017-06-14 17:27:17 +0530

    introducer name with username display on distrributor

--- lib/Model/Introducer.php
+++ lib/Model/Introducer.php
@@ -5,4 +5,16 @@ namespace xavoc\mlm;
 
 class Model_Introducer extends Model_Distributor {
 	public $table_alias =  'introducer';
+	function init(){
+		parent::init();
+		$this->addExpression('distributor_name_with_username')
+					->set($this->dsql()
+						->expr('CONCAT([0]," :: ",[1])',
+												[
+													$this->getElement('name'),
+													$this->getElement('user')
+												]
+							)
+					)->sortable(true);
+	}
 } 
