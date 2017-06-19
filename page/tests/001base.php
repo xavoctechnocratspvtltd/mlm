5f5e16117f49e8bbe09889637d5cd74931b0d2bd
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-06-19 08:31:55 +0530

    made actual test case with jayesh ji

--- page/tests/001base.php
+++ page/tests/001base.php
@@ -29,21 +29,35 @@ class page_tests_001base extends page_Tester {
         $this->resetData(true);
         $data = [
         // Company => 0,
-        'Ram'=>['introducer'=>'0','side'=>'B','on'=>'2017-05-07','kit'=>'Package A1', 'green'=>'yes'],
-        'Shyam'=>['introducer'=>'0','side'=>'B','on'=>'2017-05-07','kit'=>'Package A1', 'green'=>'yes'],
-        'Ghanshyam'=>['introducer'=>'Ram','side'=>'B','on'=>'2017-05-07','kit'=>'Package B1','green'=>'yes'],
-        'Gowrav'=>['introducer'=>'Shyam','side'=>'A','on'=>'2017-05-07'],
-        'Rakesh'=>['introducer'=>'Ghanshyam','side'=>'A','on'=>'2017-05-07'],
-        'kit-0'=>'Package A1',
+        'A'=>['introducer'=>'0','side'=>'A','on'=>'2017-05-07','kit'=>'Package B1', 'green'=>'yes'],
+        'B'=>['introducer'=>'0','side'=>'B','on'=>'2017-05-07','kit'=>'Package C1', 'green'=>'yes'],
+        'A1'=>['introducer'=>'A','side'=>'A','on'=>'2017-05-07','kit'=>'Package D1','green'=>'yes'],
+        'A2'=>['introducer'=>'A','side'=>'B','on'=>'2017-05-07','kit'=>'Package D1','green'=>'yes'],
+        'C'=>['introducer'=>'0','side'=>'A','on'=>'2017-05-07','kit'=>'Package A1','green'=>'yes'],
+        'C1'=>['introducer'=>'C','side'=>'A','on'=>'2017-05-07','kit'=>'Package B1','green'=>'yes'],
+        'C2'=>['introducer'=>'C','side'=>'B','on'=>'2017-05-07','kit'=>'Package C1','green'=>'yes'],
+        'A3'=>['introducer'=>'A','side'=>'A','on'=>'2017-05-07','kit'=>'Package D1','green'=>'yes'],
+        'D'=>['introducer'=>'B','side'=>'A','on'=>'2017-05-07','kit'=>'Package D1','green'=>'yes'],
+        'E'=>['introducer'=>'0','side'=>'B','on'=>'2017-05-07','kit'=>'Package C1','green'=>'yes'],
+        'A31'=>['introducer'=>'A3','side'=>'A','on'=>'2017-05-07'],
+        'A32'=>['introducer'=>'A3','side'=>'B','on'=>'2017-05-07'],
+        'repurchase-A31-1'=>'10000',
+        'repurchase-A32-1'=>'5000',
+        'repurchase-C2-1'=>'15000',
+        'repurchase-D-1'=>'20000',
+        'kit-0'=>'Package B1',
         'green-0'=>'2017-05-08',
-        'repurchase-Rakesh'=>'120',
-        'kit-Gowrav'=>'Package C1',
-        'green-Gowrav'=>'2017-05-08',
-        'repurchase-Gowrav'=>'240',
-        'repurchase-Ram-1'=>'36000',
-        'repurchase-Rakesh'=>'1000',
-        'closing-monthly-1'=>'2017-05-08',
-        'repurchase-Ram-2'=>'36000',
+
+
+        // 'Rakesh'=>['introducer'=>'Ghanshyam','side'=>'A','on'=>'2017-05-07'],
+        // 'repurchase-Rakesh-1'=>'120',
+        // 'kit-Gowrav'=>'Package C1',
+        // 'green-Gowrav'=>'2017-05-08',
+        // 'repurchase-Gowrav'=>'240',
+        // 'repurchase-Ram-1'=>'16000',
+        // 'repurchase-Rakesh-2'=>'1000',
+        // 'closing-monthly-1'=>'2017-05-08',
+        // 'repurchase-Ghanshyam-2'=>'136000',
         // 'ShyamIntro2'=>['introducer'=>'Shyam','side'=>'A','on'=>'2017-06-01'],
         // 'GowravIntro1'=>['introducer'=>'Gowrav','side'=>'B','on'=>'2017-06-01'],
         // 'kit-ShyamIntro2'=>'Package A1',
@@ -59,7 +73,7 @@ class page_tests_001base extends page_Tester {
 
     function prepare_company_record(){
         $this->proper_responses['test_company_record']=[
-            ['Ram'=>['d.path'=>'0B','d.month_self_bv'=>0,'p.carried_amount'=>null]]
+            ['0'=>['d.path'=>'0B','d.month_self_bv'=>0,'p.carried_amount'=>null]]
         ];
     }
 }
