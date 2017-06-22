ec759b72423de4c9717e6542d5605b2281ecf331
sinharakesh <rksinha.btech@gmail.com>
2017-06-22 19:08:29 +0530

    repurchase item with DP value added

--- page/repurchase.php
+++ page/repurchase.php
@@ -11,6 +11,6 @@ class page_repurchase extends \xepan\base\Page {
 		
 		$crud = $this->add('xepan\hr\CRUD');
 		$model= $this->add('xavoc\mlm\Model_RepurchaseItem');
-		$crud->setModel($model,['qty_unit','name','sku','display_sequence','original_price','sale_price','pv','bv','sv','capping','introducer_income','description','website_display','is_saleable']);
+		$crud->setModel($model,['qty_unit','name','sku','display_sequence','original_price','sale_price','pv','bv','sv','dp','capping','introducer_income','description','website_display','is_saleable']);
 	}
 }
