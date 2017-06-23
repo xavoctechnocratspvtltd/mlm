96b23feabd029758e179cec9df3ebec499c54d4b
sinharakesh <rksinha.btech@gmail.com>
2017-06-23 11:28:25 +0530

    update ancesestor called in hook

--- lib/Model/SalesOrder.php
+++ lib/Model/SalesOrder.php
@@ -10,23 +10,8 @@ class Model_SalesOrder extends \xepan\commerce\Model_SalesOrder {
 		// mark order invoice and paid invoice
 		// distribute repurchase value
 		// mark order complete and dispatch the goods
-
 		$sale_invoice = $this->invoice();
 		$sale_invoice->paid();
-		$distributor = $this->add('xavoc\mlm\Model_Distributor');
-		$distributor->load($this['contact_id']);
-		//invoice paid hook is not working so temporary hard code for update ancesesters
-		foreach ($this->orderItems() as $oi) {
-			$item = $this->add('xavoc\mlm\Model_Item')->load($oi['item_id']);
-			if($item['is_package']){
-				// if kit then update SV
-				$distributor->updateAnsestorsSV($item['sv']);
-			}else{
-				// update bv
-				$distributor->repurchase($item['bv']);
-			}
-		}
-
 		$this->complete();
 	}
 } 
