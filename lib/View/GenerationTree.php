174a85c69189778eb7f18ae5b5e2db41ab3d9f27
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-06-27 22:00:30 +0530

    make tree closed for generation and shortened title

--- lib/View/GenerationTree.php
+++ lib/View/GenerationTree.php
@@ -102,41 +102,8 @@ class View_GenerationTree extends \View{
 				"<br/>Gr: ". $greened_on_date. 
 				"<br/>Kit: ". ($model['kit_item']?:'') ." SV(".$model['sv'].")"."BV(".$model['bv'].")".
 				"<br/>Intro: ". $model['introducer'] .
-				"<br/><table border='1' width='100%'>
-					<tr>
-						<th> Session </th><th> Left </th><th> Right </th>
-					</tr>
-					<tr>
-						<th>SV</th><td>".$model['day_left_sv']."</td><td>".$model['day_right_sv']."</td>
-					</tr>
-					</table>
-					<div class='atk-box-small'>Gen Business: ".$model['generation_business']."</div>
-					<div class='atk-box-small'>Month Self BV: ".$model['month_self_bv']."</div>
-					<div class='atk-box-small atk-swatch-green'>Session Intros: ".$model['session_intros_amount']." /-</div>
-					<div class='atk-box-small atk-size-mega atk-swatch-green'>Downline</div>
-					<table border='1' width='100%'>
-						<tr>
-							<td>&nbsp;</td>
-							<td>Left</td>
-							<td>Right</td>
-						</tr>
-						<tr>
-							<td>Total</td>
-							<td>". $model->newInstance()->addCondition('path','like',$model['path'].'A%')->count()->getOne() ."</td>
-							<td>". $model->newInstance()->addCondition('path','like',$model['path'].'B%')->count()->getOne() ."</td>
-						</tr>
-						<tr>
-							<td>Orange</td>
-							<td>". $model->newInstance()->addCondition('path','like',$model['path'].'A%')->addCondition('ansestors_updated',false)->count()->getOne() ."</td>
-							<td>". $model->newInstance()->addCondition('path','like',$model['path'].'B%')->addCondition('ansestors_updated',false)->count()->getOne() ."</td>
-						</tr>
-						<tr>
-							<td>Green</td>
-							<td>". $model->newInstance()->addCondition('path','like',$model['path'].'A%')->addCondition('greened_on','<>',null)->addCondition('ansestors_updated',true)->count()->getOne() ."</td>
-							<td>". $model->newInstance()->addCondition('path','like',$model['path'].'B%')->addCondition('greened_on','<>',null)->addCondition('ansestors_updated',true)->count()->getOne() ."</td>
-						</tr>
-					</table>
-					";
+				"<br/>Month Self BV: ". $model['month_self_bv'];
+				
 		$str= str_replace("'", "\'", $str);
 		$str= str_replace("\n", "", $str);
 		return $str;
