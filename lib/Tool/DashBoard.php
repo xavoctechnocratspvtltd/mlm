c7341844bdef31a16a1c950899240010eed5baf7
sinharakesh <rksinha.btech@gmail.com>
2017-06-28 13:03:47 +0530

    month bv and totalbv card vieew added

--- lib/Tool/DashBoard.php
+++ lib/Tool/DashBoard.php
@@ -38,18 +38,21 @@ class Tool_DashBoard extends \xepan\cms\View_Tool{
 		$card_options = ['icon'=>'fa fa-trophy','header'=>'Current Rank','theme'=>'purple','title'=>$distributor['current_rank']?:"not achived"];
 		$col1->add('xavoc\mlm\View_Card',['options'=>$card_options]);
 
-		$card_options = ['icon'=>'fa fa-users','header'=>'Weekly Pair','theme'=>'orange','title'=>$distributor['week_pairs']?:0];
-		$col1->add('xavoc\mlm\View_Card',['options'=>$card_options]);
-
-
 		$card_options = ['icon'=>'fa fa-arrow-left','header'=>'Total Left Downline SV','theme'=>'blue','title'=>$distributor['total_left_sv']?:0];
 		$col2->add('xavoc\mlm\View_Card',['options'=>$card_options]);
 
 		$card_options = ['icon'=>'fa fa-arrow-right','header'=>'Total Right Downline SV','theme'=>'blue','title'=>$distributor['total_right_sv']?:0];
 		$col3->add('xavoc\mlm\View_Card',['options'=>$card_options]);
 
-			
-
+		$card_options = ['icon'=>'fa fa-users','header'=>'Weekly Pair','theme'=>'orange','title'=>$distributor['week_pairs']?:0];
+		$col1->add('xavoc\mlm\View_Card',['options'=>$card_options]);
+		
+		$card_options = ['icon'=>'fa fa-circle-o','header'=>'Month Self BV','theme'=>'green','title'=>$distributor['month_self_bv']?:0];
+		$col2->add('xavoc\mlm\View_Card',['options'=>$card_options]);
+		
+		$card_options = ['icon'=>'fa fa-circle-o','header'=>'Total Month BV','theme'=>'green','title'=>$distributor['total_month_bv']?:0];
+		$col3->add('xavoc\mlm\View_Card',['options'=>$card_options]);
+		
 	}
 
 	function defaultTemplate(){
