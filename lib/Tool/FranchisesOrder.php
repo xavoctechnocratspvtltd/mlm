5d25d9ea9bbcbb317958d6eec1265e373c96084e
vijay mali <guddavijay@gmail.com>
2017-06-29 18:48:21 +0530

    franchises order detail display on view

--- lib/Tool/FranchisesOrder.php
+++ lib/Tool/FranchisesOrder.php
@@ -10,11 +10,76 @@ class Tool_FranchisesOrder extends \xepan\cms\View_Tool{
 		
 		if($this->owner instanceof \AbstractController) return;
 		
-		$sale_order = $this->add('xavoc\mlm\Model_SalesOrder');
 		$this->addClass('main-box');
-		$grid = $this->add('xepan\base\Grid');
-		$grid->setModel($sale_order,['document_no','contact','net_amount']);
-		$grid->addPaginator($ipp=50);
+		$o_id = $this->app->stickyGET('order_id');
+		
+		$sale_order = $this->add('xavoc\mlm\Model_SalesOrder');
+		if($o_id)
+			$sale_order->tryLoadBy('document_no',$o_id);
 
+		$v = $this->add('View');
+		// $v->add('View_Error')->set("Order No : ".$o_id);
+
+		$f = $v->add('Form');
+		$f->addField('line','order_no');
+		$f->addSubmit('Get Detail')->addClass('btn btn-primary');
+		
+		if($f->isSubmitted()){
+			$js=[
+					$v->js()->reload(['order_id'=>$f['order_no']])
+			];
+			$f->js(null,$js)->execute();	
+		}
+		
+		if(!$sale_order->loaded()){
+			return;
+		}
+		$order_view = $v->add('View',null,null,['view/franschises-order-item','order']);
+		$order_view->setModel($sale_order);
+
+
+		$contact = $sale_order->ref('contact_id');
+		// throw new \Exception($contact['user'], 1);
+		$inv = explode('-', $sale_order['invoice_detail']);
+		$inv_no = $inv[0];
+		$inv_status = $inv[1];
+
+		$order_view->template->trySet('order_date',date('M d,Y',strtotime($sale_order['created_at'])));
+		$order_view->template->trySet('dis_user_id',$contact['user']);
+		$order_view->template->trySet('invoice_no',$inv_no);
+		$order_view->template->trySet('invoice_status',$inv_status);
+
+		if($inv_status === "Due"){
+			$order_view->template->trySet('status_class',"text-danger");
+		}else{
+			$order_view->template->trySet('status_class',"text-success");
+		}
+		
+		$order_view->add('Button',null,'btn_wrapper')->set('Dispatch')->addClass('btn btn-warning  pull-right');
+		
+		if($inv_status!=="Paid"){
+			$pay_now_btn = $order_view->add('Button',null,'btn_wrapper')->set('Pay Now')->addClass('btn btn-success  pull-right');
+			
+			if($pay_now_btn->isClicked()){
+				$sale_order->invoice()->paid();
+				$this->js(null,$v->js()->reload())->reload()->execute();
+			}	
+
+		}else{
+			$payment_detail_btn = $order_view->add('Button',null,'btn_wrapper')->set('Payment Detail')->addClass('btn btn-success  pull-right');
+			if($payment_detail_btn->isClicked()){
+				// $sale_order->invoice()->paid();
+				$this->js(null,$v->js()->reload())->reload()->execute();
+			}
+		}
+
+
+
+
+		$order_item = $this->add('xavoc\mlm\Model_QSPDetail');
+		$order_item->addCondition('qsp_master_id',$sale_order->id);
+
+		$cl = $v->add('CompleteLister',null,null,['view/franschises-order-item','order_item']);
+		$cl->setModel($order_item);
 	}
 }
