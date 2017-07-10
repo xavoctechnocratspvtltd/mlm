1bfa45e602397102b46559c1871a5c16f493d885
sinharakesh <rksinha.btech@gmail.com>
2017-07-10 10:57:00 +0530

    cron for autodailyclosing temporary disabled

--- lib/Controller/AutoDailyClosing.php
+++ lib/Controller/AutoDailyClosing.php
@@ -6,6 +6,7 @@ class Controller_AutoDailyClosing extends \AbstractController {
 	function init(){
 		parent::init();
 
+		return;
 		$closing_m = $this->add('xavoc\mlm\Model_Closing');
 		$closing_m['on_date'] = $this->app->now;
 		$closing_m['type'] = "DailyClosing";
