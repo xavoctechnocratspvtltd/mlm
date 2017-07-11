055290925f1663f9171e23a0f25a6525b79f05e9
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-07-11 11:34:31 +0530

    config based auto daily closing now

--- lib/Controller/AutoDailyClosing.php
+++ lib/Controller/AutoDailyClosing.php
@@ -9,7 +9,11 @@ class Controller_AutoDailyClosing extends \AbstractController {
 		// test if it is running on same date time like
 		// save some file with $this->app->now and date(Y-m-d H:i:s) both values some 
 		// where that we can test later on
-		return;
+
+		if($this->app->getConfig('auto_daily_closing',false)){
+			return;
+		}
+		
 		$closing_m = $this->add('xavoc\mlm\Model_Closing');
 		$closing_m['on_date'] = $this->app->now;
 		$closing_m['type'] = "DailyClosing";
