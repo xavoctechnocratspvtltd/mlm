6d5cd2af154e7ed31941c617eb2c82c1e944c456
sinharakesh <rksinha.btech@gmail.com>
2017-07-14 19:19:49 +0530

    working on franchises dispatched

--- lib/Model/AssignOrder.php
+++ lib/Model/AssignOrder.php
@@ -11,8 +11,9 @@ class Model_AssignOrder extends \xepan\base\Model_Table {
 
 		$this->hasOne('xavoc\mlm\Model_Franchises','franchises_id');
 		$this->hasOne('xavoc\mlm\Model_SalesOrder','saleorder_id');
+		$this->hasOne('xepan\base\Model_User','created_by_id');
 		$this->addField('created_at')->type('datetime')->defaultValue($this->app->now);
-		$this->addField('created_by_id');
+		// $this->addField('created_by_id');
 
 		// status from order status
 		$this->addExpression('status')->set($this->refSQL('saleorder_id')->fieldQuery('status'));
--- lib/Tool/FranchisesDispatch.php
+++ lib/Tool/FranchisesDispatch.php
@@ -11,6 +11,48 @@ class Tool_FranchisesDispatch extends \xepan\cms\View_Tool{
 		if($this->owner instanceof \AbstractController) return;
 		
 		$this->addClass('main-box');
-		
+		$franchises = $franchises = $this->add('xavoc\mlm\Model_Franchises');
+		$franchises->loadLoggedIn();
+
+		$assign_order = $this->add('xavoc\mlm\Model_AssignOrder');
+		$assign_order->addCondition('franchises_id',$franchises->id);
+		$assign_order->addCondition('status','<>','Completed');
+
+		$assign_orders_array = $assign_order->_dsql()->del('fields')->field('saleorder_id')->getAll();
+		$assign_orders_array = iterator_to_array(new \RecursiveIteratorIterator(new \RecursiveArrayIterator($assign_orders_array)),false);
+
+		$saleorder = $this->add('xavoc\mlm\Model_SalesOrder');
+		$saleorder->getElement('document_no')->caption('Order No.');
+		$saleorder->getElement('contact')->caption('customer');
+
+		$saleorder->addCondition('id',$assign_orders_array);
+		$saleorder->setOrder('id','desc');
+
+		$grid = $this->add('xepan\base\Grid');
+		$grid->setModel($saleorder,['document_no','contact','shipping_address','shipping_city','shipping_state','shipping_country','net_amount']);
+
+		$grid->addHook('formatRow',function($g){
+			$g->current_row_html['shipping_address'] = $g->model['shipping_address']."<br/>".$g->model['shipping_city']."<br/>".$g->model['shipping_state']."<br/>".$g->model['shipping_country'];
+		});
+		$grid->removeColumn('shipping_country');
+		$grid->removeColumn('shipping_state');
+		$grid->removeColumn('shipping_city');
+
+		$grid->add('VirtualPage')
+			->addColumn('Dispatch')
+			->set(function($page){
+				$id = $_GET[$page->short_name.'_id'];
+				$page->add('Text')->set('ID='.$id);
+
+				$x= $this->add('xepan\commerce\Model_Store_OrderItemDispatch')
+					->tryLoadAny();
+
+				$x->page_dispatch($page);
+
+				// warehouse try load store transaction entry with status received
+
+			});
+
+
 	}
 }
