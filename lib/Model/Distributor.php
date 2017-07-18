279d8492765b140ce9e0d7558ba70d95267bc93b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-07-18 22:43:17 +0530

    Report Tool added

--- lib/Model/Distributor.php
+++ lib/Model/Distributor.php
@@ -24,7 +24,7 @@ class Model_Distributor extends \xepan\commerce\Model_Customer {
 		// $this->getElement('pan_no')->display(array('form'=>'xavoc\mlm\PanNumber'));
 		$dist_j = $this->join('mlm_distributor.distributor_id');
 
-		$dist_j->hasOne('xavoc\mlm\Sponsor','sponsor_id')->display(['form'=>'xepan\base\Basic'])->defaultValue(0);
+		$dist_j->hasOne('xavoc\mlm\Sponsor','sponsor_id')->display(['form'=>'xepan\base\Basic'])->defaultValue(0)->caption('Placement Parent');
 		$dist_j->hasOne('xavoc\mlm\Introducer','introducer_id','distributor_name_with_username')->display(['form'=>'xepan\base\Basic'])->defaultValue(0);
 
 		$dist_j->hasOne('xavoc\mlm\Left','left_id')->display(['form'=>'xepan\base\DropDownNormal']);
---
+++ lib/View/Report/Distributor/Introductions.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class xavoc\mlm;
+
+class View_Report_Distributor_Introductions extends \View{
+	function init(){
+		parent::init();
+	}
+
+	function setModel($model){
+		// add search related form
+		$form = $this->add('Form');
+		$form->addField('search_distributor');
+
+		$downline = $this->add('xavoc\mlm\Model_Distributor');
+		$downline->addCondition('introducer_id','like',$model->id);
+
+		if($_GET['search_distributor']){
+			$downline>addCondition([['user',$_GET['search_distributor']],['name','like','%'.$_GET['search_distributor'].'%'],['id',$_GET['search_distributor']]]);
+		}
+		
+		$grid = $this->add('Grid');
+		$grid->setModel($downline,['name','sponsor','introducer','status']);
+
+		// reload self view with form values
+		if($form->isSubmitted()){
+			$this->js()->reload(['search_distributor'=>$form['search_distributor']])->execute();
+		}
+		return parent::setModel($model);
+	}
+}
