be47a48e866ef79f57186b31e21596b6961fa226
sinharakesh <rksinha.btech@gmail.com>
2017-07-21 18:42:47 +0530

    distributor list report added

--- page/report.php
+++ page/report.php
@@ -16,6 +16,7 @@ class page_report extends \xepan\base\Page {
 		$this->app->side_menu->addItem(['Plan Wise Sale','icon'=>'fa fa-circle-o','badge'=>[0,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xavoc_dm_report_planwisesale'))->setAttr(['title'=>'Plan wise sale report']);
 		$this->app->side_menu->addItem(['Id Wise Sale','icon'=>'fa fa-circle-o','badge'=>[0,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xavoc_dm_report_distributor'))->setAttr(['title'=>'Distributor Id wise sale report']);
 		$this->app->side_menu->addItem(['Dispatch Id wise','icon'=>'fa fa-circle-o','badge'=>[0,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xavoc_dm_report_dispatch'))->setAttr(['title'=>'Distributor Report Id Wise']);
+		$this->app->side_menu->addItem(['Distributor List','icon'=>'fa fa-circle-o','badge'=>[0,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xavoc_dm_report_distributorlist'))->setAttr(['title'=>'Distributor List']);
 		// $this->app->side_menu->addItem(['Franchises Sale','icon'=>'fa fa-shopping-cart text-success','badge'=>[0,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_dm_report_franchisessale'))->setAttr(['title'=>'Franchises Sales Report']);
 	
 	}
