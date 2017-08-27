56e9ffc07572fd892659b4a954cd534e91e1ad68
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-08-27 21:04:41 +0530

    manage item category

--- lib/Model/RepurchaseItem.php
+++ lib/Model/RepurchaseItem.php
@@ -6,7 +6,7 @@ namespace xavoc\mlm;
 class Model_RepurchaseItem extends \xavoc\mlm\Model_Item{
 	
 	public $actions = [
-		'Published'=>['view','edit','delete','detail','unpublish'],
+		'Published'=>['view','edit','delete','detail','unpublish','addCategory'],
 		'UnPublished'=>['view','edit','delete','publish']
 	];
 
@@ -22,6 +22,12 @@ class Model_RepurchaseItem extends \xavoc\mlm\Model_Item{
 
 	function detail(){
 		$this->app->js()->univ()->newWindow($this->app->url('xepan_commerce_itemdetail',['action'=>'edit','document_id'=>$this->id]))->execute();
+	}
+
+	function page_addCategory($page){
+		$cat_m = $this->add('xepan\commerce\Model_CategoryItemAssociation')->addCondition('item_id',$this->id);
+		$crud = $page->add('xepan\base\CRUD');
+		$crud->setModel($cat_m,['category_id'],['category']);
 	}
 
 }
