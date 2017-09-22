24b68811c341dfc5a050435e11db3ab60de56c8c
sinharakesh <rksinha.btech@gmail.com>
2017-09-22 17:51:06 +0530

    shopping menu working

--- lib/Tool/MyOrder.php
+++ lib/Tool/MyOrder.php
@@ -71,7 +71,27 @@ class Tool_MyOrder extends \xavoc\mlm\Tool_Distributor{
 
 
 	function courierDetail(){
-		$this->add('View')->set('we are working on it');
+		// $my_order = $this->order;
+		// $grid = $this->add('xepan\base\Grid');
+		// $grid->addSno('Sr. No.');
+		// $grid->setModel($my_order,['created_at','document_no','items','gross_amount','tax_amount','net_amount','details']);
+		// $self = $this;
+
+		// $this->vp = $this->add('VirtualPage')->set(function($p)use($self){
+		// 	$p->api->stickyGET('sales_order_id');
+		// 	$o = $this->add('xavoc\mlm\Model_QSPDetail')->addCondition('qsp_master_id',$_GET['sales_order_id']);
+		// 	$o->getElement('amount_excluding_tax')->caption('amount');
+		// 	$order = $p->add('xepan\base\Grid');
+		// 	$order->setModel($o,['name','item_sku','price','quantity','amount_excluding_tax','tax_amount','total_amount']);
+		// });
+		// $grid->addColumn('details');
+		// $grid->addMethod('format_details',function($g,$f){
+		// 	$g->current_row_html[$f] = '<a class="btn btn-primary" href="#na" onclick="javascript:'.$g->js()->univ()->frameURL('Order Items '. $g->model['sales_order'], $this->api->url($this->vp->getURL(),array('sales_order_id'=>$g->model->id))).'">See Details</a>';
+		// });
+		// $grid->addFormatter('details','details');
+
+		// $grid->addQuickSearch(['document_no','created_at','status']);	
+		// $grid->addPaginator(10);
 	}
 
 	function invoiceDetail(){
