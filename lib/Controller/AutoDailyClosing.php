6ef3991c9cb7da7045e83b5e9939963300a9f5d3
sinharakesh <rksinha.btech@gmail.com>
2017-11-05 14:15:07 +0530

    closing related configuration updated

--- lib/Controller/AutoDailyClosing.php
+++ lib/Controller/AutoDailyClosing.php
@@ -10,7 +10,22 @@ class Controller_AutoDailyClosing extends \AbstractController {
 		// save some file with $this->app->now and date(Y-m-d H:i:s) both values some 
 		// where that we can test later on
 
-		if($this->app->getConfig('auto_daily_closing',false)){
+		$c_s_m = $this->add('xepan\base\Model_ConfigJsonModel',
+				[
+					'fields'=>[
+								'mark_green_stopped'=>'checkbox',
+								'enable_closing'=>'checkbox',
+								'new_registration_stopped'=>'checkbox',
+								'auto_daily_closing'=>'checkbox',
+								'weekly_closing_day'=>'DropDown',
+								'monthly_closing_date'=>'DropDown',
+							],
+						'config_key'=>'CLOSING_RELATED_CONFIG',
+						'application'=>'mlm'
+				]);
+		$c_s_m->tryLoadAny();
+
+		if($this->app->getConfig('auto_daily_closing',false) AND $c_s_m['auto_daily_closing']){
 			echo "auto closing off in config, returning";
 			return;
 		}
